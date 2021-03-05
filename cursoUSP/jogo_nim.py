9c5ef09618fb58f6dde08ec160ebaab0e6369977
romulogm <romulogm@gmail.com>
2021-03-05 17:44:28 -0300

    Parece que funciona!

--- cursoUSP/jogo_nim.py
+++ cursoUSP/jogo_nim.py
@@ -5,12 +5,14 @@ def computador_escolhe_jogada(n, m):
     #ao jogador.Caso isso não seja possível, deverá tirar o número
     #máximo de peças possíveis.
     retirar = m
-    resto = n - retirar
-    if resto % (m+1) == 0:
-        print("O computador tirou", retirar, "peças.")
-        return retirar
-    while resto % (m+1) != 0:
-        retirar = retirar - 1
+
+    while retirar >= 0:
+        resto = n - retirar
+        if resto % (m+1) == 0:
+            print("O computador tirou", retirar, "peças.")
+            return retirar
+        if resto % (m+1) != 0:
+            retirar = retirar - 1
         if retirar == 0 and resto % (m+1) != 0:
             retirar = m
             print("O computador tirou", retirar, "peças.")
@@ -29,10 +31,19 @@ def usuario_escolhe_jogada(n, m):
 
 def partida():
     # inicio
+    vitoria = 0
     print("**** Rodada ****")
-    # perguntar valores de n e m
+    # perguntar valor de n
     n = int(input("Quantas peças?"))
+    while n <= 0:
+        n = int(input("Quantas peças?"))
+    # perguntar valor de m
     m = int(input("Limite de peças por jogada?"))
+    while m > n:
+        m = int(input("Limite de peças por jogada?"))
+    while m <= 0:
+        m = int(input("Limite de peças por jogada?"))
+
     # decidir quem inicia o game e atualizar o valor de n
     if (m + 1) % n == 0:
         print("Você começa!")
@@ -43,26 +54,26 @@ def partida():
         print("Computador começa!")
     #laço para intercalar as jogadas
     while inicio == 1 and n > 0:
-        n = n - usuario_escolhe_jogada(n, m)
+        n = n - int(usuario_escolhe_jogada(n, m))
         if n == 0:
-            vitoria = usuario
+            vitoria = 2
             print("Fim do jogo! O usuário ganhou!")
             return vitoria
-        n = n - computador_escolhe_jogada(n, m)
+        n = n - int(computador_escolhe_jogada(n, m))
         if n == 0:
-            vitoria = pc
+            vitoria = 1
             print("Fim do jogo! O computador ganhou!")
             return vitoria
     while inicio == 0 and n > 0:
-        n = n - computador_escolhe_jogada(n, m)
+        n = n - int(computador_escolhe_jogada(n, m))
         print("Agora restam ", n, "peças no tabuleiro.")
         if n == 0:
-            vitoria = pc
+            vitoria = 1
             print("Fim do jogo! O computador ganhou!")
             return vitoria
-        n = n - usuario_escolhe_jogada(n, m)
+        n = n - int(usuario_escolhe_jogada(n, m))
         if n == 0:
-            vitoria = usuario
+            vitoria = 2
             print("Fim do jogo! O usuário ganhou!")
             return vitoria
     #corrigir: quando tira mais peças e N fica negativo
@@ -72,20 +83,21 @@ def campeonato():
     #chamar função partida() 3 vezes
     score_computador = 0
     score_usuario = 0
+    vitoria = 0
     partida()
-    if vitoria == pc:
+    if vitoria == 1:
         score_computador = score_computador + 1
-    if vitoria == usuario:
+    if vitoria == 2:
         score_usuario = score_usuario + 1
     partida()
-    if vitoria == pc:
+    if vitoria == 1:
         score_computador = score_computador + 1
-    if vitoria == usuario:
+    if vitoria == 2:
         score_usuario = score_usuario + 1
     partida()
-    if vitoria == pc:
+    if vitoria == 1:
         score_computador = score_computador + 1
-    if vitoria == usuario:
+    if vitoria == 2:
         score_usuario = score_usuario + 1
     print("**** Final do campeonato! ****")
     print("Placar: Você", score_usuario, "X", score_computador, "Computador")
