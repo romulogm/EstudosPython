3dead838d94e12d601e5f73a4b967b150083aae6
romulogm <romulogm@gmail.com>
2021-03-22 22:37:25 -0300

    Commit .igitignore

--- cursoUSP/jogo_nim.py
+++ cursoUSP/jogo_nim.py
@@ -1,22 +1,36 @@
 # n = número de peças inicial
 # m = número máximo de peças máximo de retirar
-def computador_escolhe_jogada(n, m):
-    #deixar sempre um número de peças que seja múltiplo de (m+1)
-    #ao jogador.Caso isso não seja possível, deverá tirar o número
-    #máximo de peças possíveis.
-    retirar = m
+   
+#deixar sempre um número de peças que seja múltiplo de (m+1)
+#ao jogador.Caso isso não seja possível, deverá tirar o número
+#máximo de peças possíveis.
 
-    while retirar >= 0:
-        resto = n - retirar
-        if resto % (m+1) == 0:
-            print("O computador tirou", retirar, "peças.")
-            return retirar
-        if resto % (m+1) != 0:
-            retirar = retirar - 1
-        if retirar == 0 and resto % (m+1) != 0:
-            retirar = m
-            print("O computador tirou", retirar, "peças.")
-            return retirar
+def computador_escolhe_jogada(total, m):
+    if test_divis(total, m, m) == True:
+        print("O computador tirou", m, "peças.")
+        return m
+    
+    if total <= m: 
+        print("O computador tirou", total, "peças.")
+        return total
+
+    retirar = m-1
+    while test_divis(total, m, retirar) == False:
+        if retirar == 0:
+            print("O computador tirou", m, "peças.")
+            return m
+
+        retirar = retirar - 1
+
+    print("O computador tirou", retirar, "peças.")    
+    return retirar
+    
+    
+def test_divis(n,m, retirar):
+    if (n - retirar) % (m+1) == 0:
+        return True
+    else:
+        return False
 
 def usuario_escolhe_jogada(n, m):
     retirar = int(input("Quantas peças você vai tirar?"))
@@ -24,9 +38,20 @@ def usuario_escolhe_jogada(n, m):
         print("Você tirou", retirar, "peças.")
         return retirar
     while retirar <= 0:
-        retirar = int(input("Quantas peças você vai tirar?"))
+        retirar = 1
+        print("Você tirou", retirar, "peças.")
+        return retirar
     while retirar > m:
-        retirar = int(input("Quantas peças você vai tirar?"))
+        retirar = m
+        print("Você tirou", retirar, "peças.")
+        return retirar
+
+
+def erro_input(total, input): 
+    if input <= 0: 
+        return True
+    if input < total:
+        return True
 
 
 def partida():
@@ -43,9 +68,9 @@ def partida():
         m = int(input("Limite de peças por jogada?"))
     while m <= 0:
         m = int(input("Limite de peças por jogada?"))
-
+        
     # decidir quem inicia o game e atualizar o valor de n
-    if (m + 1) % n == 0:
+    if n % (m + 1)  == 0:
         print("Você começa!")
         inicio = 1
         usuario_escolhe_jogada(n, m)
@@ -103,18 +128,22 @@ def campeonato():
     print("Placar: Você", score_usuario, "X", score_computador, "Computador")
 
 def main():
-    #criar função main chamando a função que dá início ao game
-    print("Bem vindo ao jogo do NIM! Escolha: ")
-    print("1 - para jogar com uma partida isolada")
-    print("2 - para jogar um campeonato")
-    jogo = int(input())
-    while jogo == 1 or jogo == 2:
-        if jogo == 1:
-            print("Você escolheu uma partida isolada!")
-            campeonato()
-        if jogo == 2:
-            print("Você escolheu campeonato")
-            partida()
-    if jogo != 1 or 2:
-        int(input())
+   #criar função main chamando a função que dá início ao game
+   print("Bem vindo ao jogo do NIM! Escolha: ")
+   print("1 - para jogar com uma partida isolada")
+   print("2 - para jogar um campeonato")
+   jogo = int(input())
+   while jogo == 1 or jogo == 2:
+       if jogo == 1:
+           print("Você escolheu uma partida isolada!")
+           campeonato()
+       if jogo == 2:
+           print("Você escolheu campeonato")
+           partida()
+   if jogo != 1 or 2:
+       int(input())
+
+# def main():
+#     print(test_divis(13, 4, 3))
+
 main()
--- cursoUSP/teste.py
+++ cursoUSP/teste.py
@@ -1,26 +1,37 @@
-def maior_primo(x):
-        maior = 2
-        while ePrimo(maior) == True:
-                maior = maior + 1
-        if maior == x:
-                print(maior)
+# n = número de peças inicial
+# m = número máximo de peças máximo de retirar
+   
+#deixar sempre um número de peças que seja múltiplo de (m+1)
+#ao jogador.Caso isso não seja possível, deverá tirar o número
+#máximo de peças possíveis.
 
-def ePrimo(k):
-    if k % 2 == 0:
-        return False
-    elif k % 3 == 0:
-        return False
-    elif k % 5 == 0:
-        return False
-    elif k % 7 == 0:
-        return False
-    elif k % 11 == 0:
-        return False
-    elif k % 13 == 0:
-        return False
-    elif k % 17 == 0:
-        return False
-    elif k % 23 == 0:
-        return False
-    else:
-        return True
+def computador_escolhe_jogada(n, m):
+    
+    def test_divis(n,m):
+        
+        if (n - m) % (m+1) == 0:
+            return True
+        
+        if(n - m) % (m+1) != 0:
+            return False
+        
+    retirar = m
+
+    if test_divis(n, retirar) == True:
+        print("O computador tirou", retirar, "peças.")
+        return retirar
+    
+    if n <= m: 
+        print("O computador tirou", n, "peças.")
+        return n
+        
+    while test_divis(n, retirar) == False:
+        retirar = retirar - 1
+
+        if retirar == 0:
+            print("O computador tirou", m, "peças.")
+            return m
+
+        if test_divis(n, retirar) == True:
+            print("O computador tirou", retirar, "peças.")
+            return retirar
